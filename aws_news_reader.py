cbc8ff8970b4fd28ba5dd48b8f7bd79d866122d8
Stephen Lizcano <stephen.lizcano@gmail.com>
2017-05-20 17:00:15 +0200

    Streamlined changes
    Customs URLs vs having to change code, better stitching of audio with
    page titles

--- aws_news_reader.py
+++ aws_news_reader.py
@@ -9,17 +9,14 @@ Created on Fri May 19 15:42:54 2017
 
 from boto3 import Session
 from readability import Document
-import requests, re
+import requests, re, sys, time, os
 from botocore.exceptions import BotoCoreError, ClientError
 from bs4 import BeautifulSoup 
 from contextlib import closing
-import os
-import sys
 import subprocess
 from tempfile import gettempdir
 from os.path import expanduser
 from glob import *
-import time
 from pydub import *
 
 
@@ -40,7 +37,7 @@ def news_parser(url):
     
     #now split strength by 1450 char length (max in AWS is 1500)
     n = 1450
-    return [parsed[i:i+n] for i in range(0, len(parsed), n)]
+    return [parsed[i:i+n] for i in range(0, len(parsed), n)], doc.title()
         
 def speech_generator(text, index):
     
@@ -80,12 +77,12 @@ def speech_generator(text, index):
         sys.exit(-1)
     
     # Play the audio using the platform's default player
-    if sys.platform == "win32":
-        os.startfile(output)
-    else:
-        # the following works on Mac and Linux. (Darwin = mac, xdg-open = linux).
-        opener = "open" if sys.platform == "darwin" else "xdg-open"
-    subprocess.call([opener, output])
+    #if sys.platform == "win32":
+    #    os.startfile(output)
+    #else:
+    #    # the following works on Mac and Linux. (Darwin = mac, xdg-open = linux).
+    #    opener = "open" if sys.platform == "darwin" else "xdg-open"
+    #subprocess.call([opener, output])
 
 
 # Create a client using the credentials and region defined in the [adminuser]
@@ -93,30 +90,38 @@ def speech_generator(text, index):
 session = Session(profile_name="default")
 polly = session.client("polly")
 
-url = 'http://www.newyorker.com/culture/culture-desk/how-roger-ailes-degraded-the-tone-of-public-life-in-america?intcid=mod-latest'
-procd_text = news_parser(url)
-#news_parser('http://www.newyorker.com/magazine/2013/08/26/whats-wrong-with-me')
+#This receives the URL, and sends it to the news parser
+url = input("Please paste the url for processing:")
+procd_text, title = news_parser(url)
 
+
+#Pause to make sure that the information is read correctly
 print(procd_text)
-#pause to make sure it's parsed ok
-time.sleep(10)
+decision = input("Continue? [y/n]")
+if decision == 'n':
+	print('Goodbye')
+	sys.exit()
+
+#Parse lines, and generate speech
+print("Generating speech from Polly...")
 for line_index, line in enumerate(procd_text):
     speech_generator(line, line_index)
 
-#load temp files, then combine using ffmpeg
+#Load temp files, then combine using ffmpeg
 playlisst_songs = sorted(glob(expanduser("~")+'/audio/incomplete/*.mp3'), key=os.path.getmtime)
 newlist = []
 for el in playlisst_songs:
     newlist.append(AudioSegment.from_mp3(el))
     
-combined = AudioSegment.empty()
-    
+#Generate a blank audio file, and concatenate the pieces of audio, and export
+combined = AudioSegment.empty() 
 for song in newlist:
     combined += song
 
-combined.export(expanduser("~")+"/audio/total.mp3", bitrate='160k',format="mp3")
+print('Combining files, and removing trash...')
+combined.export(expanduser("~")+"/audio/"+ title + ".mp3", bitrate='160k',format="mp3")
 
-#cleanup the temps
+#Erase the temps
 for el in playlisst_songs:
 	os.remove(el)
 
